0cf84d978fbea04da0ae30e43bba0989fd32bfbc
Kim Hanchul <hanchul.kim@mackerly.com>
2021-05-11 23:54:37 +0900

    * 관리자 화면 완성 * 데이터 JSON으로 저장/불러오기

--- client/dashboard.js
+++ client/dashboard.js
@@ -5,31 +5,76 @@
 
   feather.replace()
 
+  function loadData() {
+    return window.fetch('/api/getLogs')
+      .then(res => res.json())
+      .then(logs => {
+        return logs.map(log => {
+          return {
+            ...log,
+            reportTime: new Date(log.reportTime)
+          }
+        })
+      })
+      .then(logs => {
+      return logs.sort((a, b) => {
+          return b.reportTime.getTime() - a.reportTime.getTime()
+        })
+      })
+      .then(logs => {
+        const logTableBody = document.getElementById('report-logs')
+        let logElements = ''
+
+        const graphData = {}
+        const days = [...new Set(logs.map(log => log.reportTime.toLocaleDateString()))]
+        days.forEach(day => {
+          graphData[day] = logs.filter(log => {
+            return log.reportTime.toLocaleDateString() === day
+          })
+        })
+
+        chart.data.labels = []
+        chart.data.datasets.forEach(dataset => {
+          dataset.data = []
+        })
+        days.reverse().forEach(day => {
+          chart.data.labels.push(day);
+          chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(graphData[day].length);
+          });
+        })
+        chart.update();
+
+        logs.forEach(log => {
+          const userName = log.userName
+          const logTime = log.reportTime.toLocaleString('ko-KR')
+          const newAlert = new Date().getTime() - log.reportTime.getTime() < 3600 * 1000
+            ? '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff0000" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">\n' +
+            '    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>\n' +
+            '  </svg>'
+            : ''
+
+          logElements +=
+            `<tr>\n` +
+            `    <td>${newAlert}</td>\n` +
+            `    <td>${log.deviceId}</td>\n` +
+            `    <td>${userName}</td>\n` +
+            `    <td>${logTime}</td>\n` +
+            `</tr>`
+        })
+        logTableBody.innerHTML = logElements
+      })
+  }
+
   // Graphs
   var ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
+  var chart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: [
-        '5월 8일',
-        '5월 9일',
-        '5월 10일',
-        '5월 11일',
-        '5월 12일',
-        '5월 13일',
-        '5월 14일'
-      ],
+      labels: [],
       datasets: [{
-        data: [
-          2,
-          1,
-          3,
-          2,
-          4,
-          2,
-          0
-        ],
+        data: [],
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
@@ -51,4 +96,9 @@
       }
     }
   })
+
+  loadData()
+  setInterval(() => {
+    loadData()
+  }, 1000)
 })()
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fs from 'fs'
 import path from 'path'
 import mime from 'mime-types'
 
@@ -7,47 +8,63 @@ let port = 3000
 const __dirname = path.resolve(path.dirname(''))
 
 let logs = []
+fs.readFile(__dirname + '/logs.json', (err, data) => {
+  if (!err) {
+    logs = JSON.parse(data.toString()).map((it) => {
+      return {
+        ...it,
+        reportTime: new Date(it.reportTime)
+      }
+    })
+  }
+})
 
 function createDefaultResponse() {
-    return {
-        "responseCode": 200,
-        "message": ""
-    }
+  return {
+    'responseCode': 200,
+    'message': ''
+  }
 }
 
 app.all('/client', ((req, res) => {
-    res.sendFile(__dirname + '/client/index.html')
+  res.sendFile(__dirname + '/client/index.html')
 }))
 
 app.all('/client/:fileName', ((req, res) => {
-    let fileName = req.params.fileName
-    let contentType = mime.lookup(fileName)
-    if (!contentType) {
-        contentType = 'application/octet-stream';
-    }
+  let fileName = req.params.fileName
+  let contentType = mime.lookup(fileName)
+  if (!contentType) {
+    contentType = 'application/octet-stream'
+  }
 
-    res.setHeader('Content-Type', contentType)
-    res.sendFile(__dirname + '/client/' + fileName)
+  res.setHeader('Content-Type', contentType)
+  res.sendFile(__dirname + '/client/' + fileName)
 }))
 
 app.all('/api/reportTheft', ((req, res) => {
-    const deviceId = req.query.deviceId
-    const log = {
-        deviceId: deviceId,
-        reportTime: new Date()
-    }
-    logs.push(log)
+  const deviceId = req.query.deviceId
+  const userName = req.query.userName
+  const log = {
+    deviceId: deviceId,
+    userName: userName,
+    reportTime: new Date()
+  }
+  logs.push(log)
 
-    const defaultResponse = createDefaultResponse()
-    const response = { ...defaultResponse, ...log }
-    res.send(response)
+  const defaultResponse = createDefaultResponse()
+  const response = {...defaultResponse, ...log}
+  res.send(response)
+
+  fs.writeFile(__dirname + '/logs.json', JSON.stringify(logs), err => {})
 }))
 
 app.all('/api/getLogs', ((req, res) => {
-    res.send(logs)
+  res.send(logs.sort((a, b) => {
+    return b.reportTime.getTime() - a.reportTime.getTime()
+  }))
 }))
 
 
 const server = app.listen(port, () => {
-    console.log(`server is running on ${port}`)
+  console.log(`server is running on ${port}`)
 })
